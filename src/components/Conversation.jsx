b05428eb6ae2c46734a1015be2a15bbd233be084
Tess Lockey <84677774+treskey@users.noreply.github.com>
2024-03-20 14:42:03 -0700

    changed modalContainer to MUI Container to center conversation ui
    Co-authored-by: Allen Lee <6207als@gmail.com>
    Co-authored-by: Rachele Lang <rachelelang7672@gmail.com>
    Co-authored-by: Weston Ludeke <westonludeke@gmail.com>

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -5,13 +5,13 @@ import Comments from "./Comments";
 function Conversation({ comments, onCreateComment }) {
   const [newComment, setNewComment] = useState("");
 
-	return (
-		<div id="conversation-container">
+  return (
+    <div id="conversation-container">
       <h1>campfire conversation</h1>
-      <Comments comments={comments}/>
+      <Comments comments={comments} />
       <div className="editorWrapper">
-          <Editor onCreateComment={onCreateComment}/>
-        </div>
+        <Editor onCreateComment={onCreateComment} />
+      </div>
       {/* <form onSubmit={handleSubmit}>
         <label htmlFor="newComment">New Comment:</label>
         <input
@@ -23,7 +23,7 @@ function Conversation({ comments, onCreateComment }) {
         <button type="submit">Post Comment</button>
       </form> */}
     </div>
-	)
+  );
 }
 
-export default Conversation
+export default Conversation;
