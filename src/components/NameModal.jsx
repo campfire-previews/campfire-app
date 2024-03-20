28d32018ce99ab04fe98c30ec7f812b59f8ccc2f
westonludeke <westonludeke@gmail.com>
2024-03-20 17:27:08 -0500

    remove in-line CSS from NameModal.jsx component as it's now moved to the App.css file
    Co-authored-by: Rachele Lang <rachelelang7672@gmail.com>
    Co-authored-by: Tess Lockey <84677774+treskey@users.noreply.github.com>
    Co-authored-by: Allen Lee <6207als@gmail.com>

--- src/components/NameModal.jsx
+++ src/components/NameModal.jsx
@@ -9,8 +9,7 @@ const NameModal = ({ isVisible, onSubmit }) => {
   };
 
   const handleOutsideClick = (event) => {
-    // Check if the click is outside the modal content using the ID
-    if (!event.target.closest('#modal-content')) {
+    if (!event.target.closest('#name-modal-content')) {
       onSubmit(name);
     }
   };
@@ -27,9 +26,9 @@ const NameModal = ({ isVisible, onSubmit }) => {
   if (!isVisible) return null;
 
   return (
-    <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'rgba(0, 0, 0, 0.5)', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <div id="modal-content" style={{ backgroundColor: 'white', padding: 20 }}>
-        <h2>Howdy!  Enter Your Name:</h2>
+    <div className="name-modal-overlay">
+      <div id="name-modal-content" className="name-modal-content">
+        <h2>Howdy! Enter Your Name:</h2>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         <button onClick={handleSubmit}>Submit</button>
       </div>
