ca835b10b4095cf86c5b60b2eccff331548c310d
westonludeke <westonludeke@gmail.com>
2024-03-20 17:18:09 -0500

    add handleOutsideClick function to NameModal.jsx to close NameModal if user clicks outside of modal
    Co-authored-by: Rachele Lang <rachelelang7672@gmail.com>
    Co-authored-by: Tess Lockey <84677774+treskey@users.noreply.github.com>
    Co-authored-by: Allen Lee <6207als@gmail.com>

--- src/components/NameModal.jsx
+++ src/components/NameModal.jsx
@@ -1,17 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const NameModal = ({ isVisible, onSubmit }) => {
   const [name, setName] = useState('');
 
   const handleSubmit = () => {
+    console.log('username: ', name);
     onSubmit(name);
   };
+
+  const handleOutsideClick = (event) => {
+    // Check if the click is outside the modal content using the ID
+    if (!event.target.closest('#modal-content')) {
+      onSubmit(name);
+    }
+  };
+
+  useEffect(() => {
+    if (isVisible) {
+      document.addEventListener('click', handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [isVisible]);
 
   if (!isVisible) return null;
 
   return (
     <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'rgba(0, 0, 0, 0.5)', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <div style={{ backgroundColor: 'white', padding: 20 }}>
+      <div id="modal-content" style={{ backgroundColor: 'white', padding: 20 }}>
         <h2>Howdy!  Enter Your Name:</h2>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         <button onClick={handleSubmit}>Submit</button>
