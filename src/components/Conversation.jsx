41efb29e9806dd97cbb592e2d4db04c26e8d2c1a
ramlang <rachelelang7672@gmail.com>
2024-03-20 13:55:28 -0600

    removed form element to implement lexical editor component

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -5,29 +5,23 @@ import Comments from "./Comments";
 function Conversation({ comments, onCreateComment }) {
   const [newComment, setNewComment] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await onCreateComment(newComment);
-    setNewComment("");
-  };
-
 	return (
 		<div id="conversation-container">
       <h1>campfire conversation</h1>
       <Comments comments={comments}/>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="newComment">New Comment:</label>
-        <div className="editorWrapper">
-          <Editor />
+      <div className="editorWrapper">
+          <Editor onCreateComment={onCreateComment}/>
         </div>
-        {/* <input
+      {/* <form onSubmit={handleSubmit}>
+        <label htmlFor="newComment">New Comment:</label>
+        <input
           id="newComment"
           type="text"
           value={newComment}
           onChange={(e) => setNewComment(e.target.value)}
-        /> */}
+        />
         <button type="submit">Post Comment</button>
-      </form>
+      </form> */}
     </div>
 	)
 }
