d06924f90f1b2489813be31bddcc8366aa18923c
ramlang <rachelelang7672@gmail.com>
2024-03-20 22:08:55 -0600

    removed form element and useState

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import Editor from "../editor/Editor";
 
 import Comments from "./Comments";
 
 function Conversation({ comments, onCreateComment }) {
-  const [newComment, setNewComment] = useState("");
-
 	return (
 		<div id="conversation-container">
       <h1>campfire conversation</h1>
@@ -13,16 +10,6 @@ function Conversation({ comments, onCreateComment }) {
       <div className="editorWrapper">
         <Editor onCreateComment={onCreateComment}/>
       </div>
-      {/* <form onSubmit={handleSubmit}>
-        <label htmlFor="newComment">New Comment:</label>
-        <input
-          id="newComment"
-          type="text"
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-        />
-        <button type="submit">Post Comment</button>
-      </form> */}
     </div>
 	)
 }
