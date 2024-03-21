fa5ab37998e5e0f13a332d34975d93746f43286c
westonludeke <westonludeke@gmail.com>
2024-03-21 10:12:28 -0500

    add form tag to name-modal-content of NameModal to handle user submit form if users presses enter/return key
    Co-authored-by: Rachele Lang <rachelelang7672@gmail.com>
    Co-authored-by: Tess Lockey <84677774+treskey@users.noreply.github.com>
    Co-authored-by: Allen Lee <6207als@gmail.com>

--- src/components/NameModal.jsx
+++ src/components/NameModal.jsx
@@ -4,6 +4,7 @@ const NameModal = ({ isVisible, onSubmit }) => {
   const [name, setName] = useState('');
 
   const handleSubmit = () => {
+    event.preventDefault();
     console.log('username: ', name);
     onSubmit(name);
   };
@@ -13,10 +14,12 @@ const NameModal = ({ isVisible, onSubmit }) => {
   return (
     <div className="name-modal-overlay">
       <div id="name-modal-content" className="name-modal-content">
-        <h1>welcome to campfire!</h1>
-        <h2>what is your name? (for comments)</h2>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        <button onClick={handleSubmit}>join the campfire</button>
+        <form onSubmit={handleSubmit}>
+          <h1>welcome to campfire!</h1>
+          <h2>what is your name? (for comments)</h2>
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          <button onClick={handleSubmit}>join the campfire</button>
+        </form>
       </div>
     </div>
   );
