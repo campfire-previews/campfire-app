37018351e6da9cbf80448098f3199aa9af1d105c
westonludeke <westonludeke@gmail.com>
2024-03-21 16:21:36 -0500

    add ability for user to close the NameModal only if the userName value is set
    Co-authored-by: Rachele Lang <rachelelang7672@gmail.com>
    Co-authored-by: Tess Lockey <84677774+treskey@users.noreply.github.com>
    Co-authored-by: Allen Lee <6207als@gmail.com>

--- src/components/NameModal.jsx
+++ src/components/NameModal.jsx
@@ -2,10 +2,29 @@ import React, { useState, useEffect } from 'react';
 
 const NameModal = ({ isVisible, onSubmit, defaultName }) => {
   const [name, setName] = useState(defaultName || '');
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     setName(defaultName);
+    setIsEditing(!!localStorage.getItem('userName'));
   }, [defaultName]);
+
+  const handleOutsideClick = (event) => {
+    if (!event.target.closest('#name-modal-content') && isEditing) {
+      onSubmit(name);
+    }
+  };
+
+  useEffect(() => {
+    if (isVisible) {
+      document.addEventListener('click', handleOutsideClick);
+    } else {
+      document.removeEventListener('click', handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [isVisible, isEditing]);
 
   const handleSubmit = () => {
     event.preventDefault();
@@ -17,7 +36,7 @@ const NameModal = ({ isVisible, onSubmit, defaultName }) => {
   if (!isVisible) return null;
 
   return (
-    <div className="name-modal-overlay">
+    <div className="name-modal-overlay" onClick={handleOutsideClick}>
       <div id="name-modal-content" className="name-modal-content">
         <form onSubmit={handleSubmit}>
           <h1>welcome to campfire!</h1>
