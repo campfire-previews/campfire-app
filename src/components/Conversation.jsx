60aa5e32f603f314283ab9324fac2a003169c3ae
ramlang <rachelelang7672@gmail.com>
2024-03-21 11:36:57 -0600

    fixed bug in Conversation component
    Co-authored-by: Allen Lee <6207ALS@gmail.com>
    Co-authored-by: Tess <84677774+treskey@users.noreply.github.com>
    Co-authored-by: Weston Ludeke <westonludeke@gmail.com>

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -2,8 +2,6 @@ import Editor from "../editor/Editor";
 import Comments from "./Comments";
 
 function Conversation({ comments, onCreateComment }) {
-  const [newComment, setNewComment] = useState("");
-
   return (
     <div id="conversation-container">
       <Comments comments={comments} />
