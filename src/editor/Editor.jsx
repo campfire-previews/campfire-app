ced2f609128f4428252d675fdcd975f0ee2e3488
ramlang <rachelelang7672@gmail.com>
2024-03-20 22:09:44 -0600

    converted output to markdown formatted string

--- src/editor/Editor.jsx
+++ src/editor/Editor.jsx
@@ -11,64 +11,18 @@ import { ListPlugin } from "@lexical/react/LexicalListPlugin";
 import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin";
 // import ImagesPlugin from "../CustomPlugins/ImagePlugin";
 import { EditorRefPlugin } from "@lexical/react/LexicalEditorRefPlugin";
-import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
-import { TRANSFORMERS } from '@lexical/markdown';
-
-
-import { CodeNode } from '@lexical/code';
-import { LinkNode } from '@lexical/link';
-import { ListNode, ListItemNode } from '@lexical/list';
-import { HeadingNode, QuoteNode } from '@lexical/rich-text';
-import { HorizontalRuleNode } from '@lexical/react/LexicalHorizontalRuleNode';
-
 import ToolbarPlugin from './ToolbarPlugin';
+import editorConfig from './editorConfig';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import { $convertToMarkdownString, TRANSFORMERS } from '@lexical/markdown';
+import { $getRoot } from "lexical";
+
 import { Button } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 import './styles.css';
-import { $getRoot } from "lexical";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-
-// Plugins
-// =======
-// Lexical React plugins are React components, which makes them
-// highly composable. Furthermore, you can lazy load plugins if
-// desired, so you don't pay the cost for plugins until you
-// actually use them.
 
 function Editor({ onCreateComment }) {
   const editorRef = useRef(null);
-
-  // intialConfig
-  // ============
-  // Configuration object for the LexicalComposer component which is
-  // an instance of an "editor".
-
-  // A property not listed is editorState which lets you set the intial
-  // state of the editor with proper JSON (shouldn't be just an empty string)
-
-  // Nodes extend the nodes that may occur in the editor by passing in
-  // an array of possible nodes. Three exposed are ElementNode, TextNode,
-  // and DecoratorNode. There is also always one RootNode per editor.
-
-  // Catch any errors that occur during Lexical updates and log them
-  // or throw them as needed. If you don't throw them, Lexical will
-  // try to recover gracefully without losing user data.
-
-  const initialConfig = {
-    namespace: "Campfire Conversation 2",
-    nodes: [
-      HorizontalRuleNode,
-      CodeNode,
-      LinkNode,
-      ListNode,
-      ListItemNode,
-      HeadingNode,
-      QuoteNode,
-    ],
-    onError: (error) => {
-      throw error;
-    },
-  };
 
   // onChange
   // =========
@@ -79,33 +33,30 @@ function Editor({ onCreateComment }) {
 
   function onChange(editorState) {
     editorState.read(() => {
-      const text = $getRoot().getTextContent();
-      console.log(text);
+      const markdown = $convertToMarkdownString(TRANSFORMERS);
+      console.log(markdown);
     });
   }
 
   const handleSubmit = async () => {
-    let text;
+    let markdown;
+    
     editorRef.current.getEditorState().read(() => {
-      text = $getRoot().getTextContent();
+      markdown = $convertToMarkdownString(TRANSFORMERS);
     })
     
-    await onCreateComment(text);
-    
-    editorRef.current.update(() => {
-      $getRoot().clear();
-      console.log(editorRef.current.getEditorState());
-    })
+    await onCreateComment(markdown);
+    editorRef.current.update(() => $getRoot().clear());
   };
 
   return (
-    <LexicalComposer initialConfig={initialConfig}>
+    <LexicalComposer initialConfig={editorConfig}>
       <div className="editor-container">
         <ToolbarPlugin />
         <div className="editor-inner">
           <RichTextPlugin
             contentEditable={<ContentEditable className="editor-input" />}
-            placeholder={<div>Chat around the campfire...</div>}
+            placeholder={<div className="editor-placeholder">Chat around the campfire...</div>}
             ErrorBoundary={LexicalErrorBoundary}
           />
           <HistoryPlugin />
