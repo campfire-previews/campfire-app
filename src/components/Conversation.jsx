c5420de373db828a1d76d88f6c3da174d8700f98
ramlang <rachelelang7672@gmail.com>
2024-03-21 23:24:48 -0600

    :sparkles: added upload image via URL
    Co-authored-by: Tess <84677774+treskey@users.noreply.github.com>
    Co-authored-by: Allen Lee <6207ALS@gmail.com>
    Co-authored-by: Weston Ludeke <westonludeke@gmail.com>

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,13 +1,11 @@
-import Editor from "../editor/Editor";
+import Editor from "../editor/components/Editor";
 import Comments from "./Comments";
 
 function Conversation({ comments, onCreateComment }) {
   return (
     <div id="conversation-container">
       <Comments comments={comments} />
-      <div id="editor-wrapper">
-        <Editor onCreateComment={onCreateComment} />
-      </div>
+      <Editor onCreateComment={onCreateComment} />
     </div>
   );
 }
---
+++ src/editor/components/Editor/index.jsx
@@ -0,0 +1,65 @@
+import React, { useRef } from "react";
+import { $getRoot } from "lexical";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { EditorRefPlugin } from "@lexical/react/LexicalEditorRefPlugin";
+import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
+import { $convertToMarkdownString } from "@lexical/markdown";
+import { ListPlugin } from "@lexical/react/LexicalListPlugin";
+import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin";
+import ImagePlugin from "../../customPlugins/ImagePlugin";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import EditorToolbar from "../EditorToolbar";
+import editorConfig from "./editorConfig";
+import { Button } from "@mui/material";
+import CUSTOM_TRANSFORMERS from "../../customPlugins/ImageTransformer";
+import "../../styles/styles.css";
+
+function Editor({ onCreateComment }) {
+  const editorRef = useRef(null);
+
+  const handleSubmit = async () => {
+    let markdown;
+
+    editorRef.current.getEditorState().read(() => {
+      markdown = $convertToMarkdownString(CUSTOM_TRANSFORMERS);
+    })
+
+    await onCreateComment(markdown);
+    editorRef.current.update(() => $getRoot().clear());
+  };
+
+  return (
+    <div id="editor-wrapper">
+      <LexicalComposer initialConfig={editorConfig}>
+        <div className="editor-container">
+          <EditorToolbar />
+          <div className="editor-inner">
+            <RichTextPlugin
+              ErrorBoundary={LexicalErrorBoundary}
+              contentEditable={<ContentEditable className="editor-input" />}
+              placeholder={
+                <div className="editor-placeholder">
+                  Chat around the campfire...
+                </div>
+              }
+              />
+            <HistoryPlugin />
+            <MarkdownShortcutPlugin transformers={CUSTOM_TRANSFORMERS} />
+            <EditorRefPlugin editorRef={editorRef} />
+            <ImagePlugin captionsEnabled={false} />
+            <ListPlugin />
+            <LinkPlugin />
+          </div>
+          <Button variant="contained" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </div>
+      </LexicalComposer>
+    </div>
+  )
+}
+
+export default Editor
