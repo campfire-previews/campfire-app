a5442c006aac1dcf1790a46bf648ea7b0f822312
westonludeke <westonludeke@gmail.com>
2024-03-26 10:32:58 -0500

    import FontAwesome CDN to index.html; add logic to collapse/uncollapse NameBanner component; add associated CSS to App.css
    Co-authored-by: Rachele Lang <rachelelang7672@gmail.com>
    Co-authored-by: Tess Lockey <84677774+treskey@users.noreply.github.com>
    Co-authored-by: Allen Lee <6207als@gmail.com>

--- src/components/NameBanner.jsx
+++ src/components/NameBanner.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NameBanner = ({ userName, onClick }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const handleToggleCollapse = (event) => {
+    event.stopPropagation();  // prevent onClick from being triggered when toggling collapse
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <div className="name-banner" onClick={onClick}>
-      visiting campfire as {userName}
+    <div className={`name-banner ${isCollapsed ? 'name-banner-collapsed' : ''}`} onClick={onClick}>
+      <button className="name-banner-collapse-toggle-button" onClick={handleToggleCollapse}>
+        <i className={`fas ${isCollapsed ? 'fa-angle-left' : 'fa-angle-right'}`}></i>
+      </button>
+      <span className={isCollapsed ? 'name-banner-hidden' : ''}>
+        visiting campfire as {userName}
+      </span>
     </div>
   );
 };
