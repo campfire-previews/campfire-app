5ecb23ef1b48fd25b8b19d8176f5c8a55af2aa97
Tess Lockey <84677774+treskey@users.noreply.github.com>
2024-03-28 14:17:17 -0700

    :sparkles: Added notfound page
    Co-authored-by: Allen Lee <6207als@gmail.com>
    Co-authored-by: Weston Ludeke <westonludeke@gmail.com>
    Co-authored-by: Rachele Lang <rachelelang7672@gmail.com>

--- src/components/SessionReplay.jsx
+++ src/components/SessionReplay.jsx
@@ -3,39 +3,40 @@ import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router";
 
 import * as rrweb from "rrweb";
-import rrwebPlayer from 'rrweb-player';
-import 'rrweb-player/dist/style.css';
-
+import rrwebPlayer from "rrweb-player";
+import "rrweb-player/dist/style.css";
 
 // 8bfd3309-4af1-4abf-8067-8468394a2d42
-function SessionReplay({  }) {
-	const { repo, issue_number, id } = useParams();
-	const playerRef = useRef(null);
+function SessionReplay({}) {
+  const { repo, issue_number, id } = useParams();
+  const playerRef = useRef(null);
 
-	useEffect(() => {
-		(async () => {
-			const response = await fetch(`https://r5mggbu5q0.execute-api.us-east-2.amazonaws.com/demo/repos/${repo}/issue_number/${issue_number}/session-replay/${id}`)
-			const { data: events } = await response.json();
-			if (events.length > 0) {
-				const replayer = new rrwebPlayer({
-					target: playerRef.current,
-					props: { events }
-				});
-				replayer.play();
-			}
-		})()
-	}, []);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `https://r5mggbu5q0.execute-api.us-east-2.amazonaws.com/demo/repos/${repo}/issue_number/${issue_number}/session-replay/${id}`
+      );
+      const { data: events } = await response.json();
+      if (events.length > 0) {
+        const replayer = new rrwebPlayer({
+          target: playerRef.current,
+          props: { events },
+        });
+        replayer.play();
+      }
+    })();
+  }, []);
 
-
-	return (
-		<>
-			<div id="playerContainer">
-				<div id="playerMain" ref={playerRef}></div>
-				<a href={`${location.origin}/${repo}/${issue_number}`}>
-					<button>Visit preview environment</button>
-				</a>	
-			</div>
-	)
+  return (
+    <>
+      <div id="playerContainer">
+        <div id="playerMain" ref={playerRef}></div>
+        <a href={`${location.origin}/${repo}/${issue_number}`}>
+          <button>Visit preview environment</button>
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default SessionReplay;
