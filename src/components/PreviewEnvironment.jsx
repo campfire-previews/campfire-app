6a9ae0cc0984e1382f164df351da120c2d1cced4
ramlang <rachelelang7672@gmail.com>
2024-03-28 13:23:52 -0600

    :sparkles :lipstick:  insert session replay link into comment; updated submit button in conversation modal
    Co-authored-by: Tess <84677774+treskey@users.noreply.github.com>
    Co-authored-by: Allen Lee <6207ALS@gmail.com>
    Co-authored-by: Weston Ludeke <westonludeke@gmail.com>
    Co-authored-by: Allen Lee <6207ALS@users.noreply.github.com>

--- src/components/PreviewEnvironment.jsx
+++ src/components/PreviewEnvironment.jsx
@@ -2,45 +2,16 @@ import { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router";
 import api from "../apiClient.js";
 import Preview from "./Preview.jsx";
-import removeBotPrefix from "../utils/removeBotComments.js";
 import FeedbackInterface from "./FeedbackInterface.jsx";
-import getUserData from "../utils/getUserData.js";
 
 function PreviewEnvironment() {
   const { repo, issue_number } = useParams();
-  const [comments, setComments] = useState([]);
   const iFrameRef = useRef(null);
-
-  useEffect(() => {
-    getUserData();
-    (async () => {
-      let comments = await api.getComments(repo, issue_number);
-      const filteredComments = removeBotPrefix(comments);
-      setComments(filteredComments);
-    })();
-  }, [repo, issue_number]);
-
-  const handleCreateComment = async (newComment, LGTM = false) => {
-    const commentData = {
-      user: localStorage.getItem("userName"),
-      comment: newComment,
-      LGTM,
-      userData: getUserData(),
-    };
-
-    const data = await api.sendComment(repo, issue_number, commentData);
-    setComments((prevState) => prevState.concat(data));
-  };
 
   return (
     <>
       <Preview repo={repo} issue_number={issue_number} iFrameRef={iFrameRef} />
-      <FeedbackInterface
-        repo={repo}
-        issue_number={issue_number}
-        comments={comments}
-        onCreateComment={handleCreateComment}
-        iFrameRef={iFrameRef}
+      <FeedbackInterface repo={repo} issue_number={issue_number} iFrameRef={iFrameRef}
       />
     </>
   );
